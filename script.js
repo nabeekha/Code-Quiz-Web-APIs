511ec1c2a54e4ca2f4bda3b6745bcf2a313d7574
nabeekha <nabeekha@umich.edu>
2023-02-19 08:48:52 -0500

    commit with screenshots in readme

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 var startButtonEl = document.querySelector("#start-quiz-button");
 var count = 0;
 var currentQuestionIndex = 0;
-// var time = questions.length * 15;
+//var time = questions.length * 15;
 var choicesEl = document.getElementById("choices");
 
 //questions[0].title = this is how you refer to the various questions and answers. Brings value and property titles in the object "questions"
@@ -12,21 +12,21 @@ var questions = [{
 }, 
 //change to reflect the values of each question. This would be the questions[1].title object
 {
-    title: "1. Javascript is an __ language?", // key value pairs
-    choices: ["Object-Oriented", "Object-Based", "Procedural", "None of the above"],
-    answer: "Object-Oriented", 
+    title: "2. Which of the following keywords is used to define a variable in Javascript?", // key value pairs
+    choices: ["var", "let", "Both A and B", "None of the above"],
+    answer: "Both A and B", 
 },
 
 {
-    title: "1. Javascript is an __ language?", // key value pairs
-    choices: ["Object-Oriented", "Object-Based", "Procedural", "None of the above"],
-    answer: "Object-Oriented", 
+    title: "3. Upon encountering empty statements, what does the Javascript Interpreter do?", // key value pairs
+    choices: ["Throws an error", "Ignores the statements", "Gives a warning", "None of the above"],
+    answer: "Ignores the statements", 
 },
 
 {
-    title: "1. Javascript is an __ language?", // key value pairs
-    choices: ["Object-Oriented", "Object-Based", "Procedural", "None of the above"],
-    answer: "Object-Oriented", 
+    title: "4. Which of the following methods can be used to display data in some form using Javascript?", // key value pairs
+    choices: ["document.write()", "console.log()", "window.alert()", "All of the above"],
+    answer: "All of the above", 
 }
 
 ];
@@ -71,6 +71,7 @@ function questionClick(event) {
     if (buttonEl.value !== questions[currentQuestionIndex].answer)
     {
         //subtract time 
+        time--;
 
         // move to next quesiton 
         currentQuestionIndex++
